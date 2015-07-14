f6326de3c7123b6a506237f0869a97804fa380f8
Clément Biron <clement.biron@gmail.com>
2015-07-14 10:27:02 +0200

    test et correction de disableEmoji

--- Doublefou/Helper/Theme.php
+++ Doublefou/Helper/Theme.php
@@ -97,13 +97,9 @@
 
 		/**
 		 * Désaticver les EMOJI
-		 * @todo a tester
 		 */
 		public static function disableEmoji(){
-
-			add_action( 'init', 'dfwp_disableEmoji' );
-
-			function dfwp_disableEmoji(){
+			add_action( 'init',function(){			
 				remove_action( 'admin_print_styles', 'print_emoji_styles' );
 				remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 				remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
@@ -111,16 +107,14 @@
 				remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
 				remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
 				remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
-				add_filter( 'tiny_mce_plugins', 'dfwp_disableEmojiconsTinymce' );
-			}
-			
-			function dfwp_disableEmojiconsTinymce( $plugins ) {
-				if ( is_array( $plugins ) ) {
-					return array_diff( $plugins, array( 'wpemoji' ) );
-				} else {
-					return array();
-				}
-			}
+				add_filter( 'tiny_mce_plugins', function(){
+					if ( is_array( $plugins ) ) {
+						return array_diff( $plugins, array( 'wpemoji' ) );
+					} else {
+						return array();
+					}
+				});
+			});
 		}
 	}
 ?>
