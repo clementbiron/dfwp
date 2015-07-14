88a6db025892e32c470d402606896b0e4561f2a3
Clément Biron <clement.biron@gmail.com>
2015-07-14 10:03:04 +0200

    Ajout de de disableEmoji() dans Theme

--- Doublefou/Helper/Theme.php
+++ Doublefou/Helper/Theme.php
@@ -94,5 +94,33 @@
 			language_attributes($doctype);
 			return ob_get_clean();
 		}
+
+		/**
+		 * Désaticver les EMOJI
+		 * @todo a tester
+		 */
+		public static function disableEmoji(){
+
+			add_action( 'init', 'dfwp_disableEmoji' );
+
+			function dfwp_disableEmoji(){
+				remove_action( 'admin_print_styles', 'print_emoji_styles' );
+				remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+				remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+				remove_action( 'wp_print_styles', 'print_emoji_styles' );
+				remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+				remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+				remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+				add_filter( 'tiny_mce_plugins', 'dfwp_disableEmojiconsTinymce' );
+			}
+			
+			function dfwp_disableEmojiconsTinymce( $plugins ) {
+				if ( is_array( $plugins ) ) {
+					return array_diff( $plugins, array( 'wpemoji' ) );
+				} else {
+					return array();
+				}
+			}
+		}
 	}
 ?>
