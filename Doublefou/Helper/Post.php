05025ecdf40a1b781a75a6936bad65fdb7ccb092
Clément Biron <clement.biron@gmail.com>
2016-09-21 05:51:01 +0200

    code style

--- Doublefou/Helper/Post.php
+++ Doublefou/Helper/Post.php
@@ -1,10 +1,11 @@
 <?php
 	
 	namespace Doublefou\Helper;
+
 	use Doublefou\Core\Singleton;
 
 	//Exit si accès direct
-	if (!defined('ABSPATH')) exit; 
+	if (!defined('ABSPATH')) exit;
 
 	/**
 	 * Post
@@ -15,18 +16,23 @@
 		
 		/**
 		 * Récupérer le permalink d'un post avec son slug
+		 *
 		 * @param string $pSlug Slug
+		 *
 		 * @return string|boolean
 		 */
 		public static function getPostLinkBySlug($pSlug)
 		{
 			$query = self::getPostBySlug($pSlug);
+
 			return get_permalink($query->post->ID);
 		}
 		
 		/**
 		 * Récupérer un post par son slug
+		 *
 		 * @param string $pSlug
+		 *
 		 * @return object
 		 */
 		public static function getPostBySlug($pSlug)
@@ -36,18 +42,20 @@
 		
 		/**
 		 * Récupérer un post par son titre
+		 *
 		 * @param string $pTitle
+		 *
 		 * @return object|null
 		 */
 		public static function getPostByTitle($pTitle, $output = OBJECT)
 		{
 			global $wpdb;
-		    $post = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM $wpdb->posts WHERE post_title = %s AND post_type='post'", $page_title ), $output);
-		    
-		    if ($post)
-		        return $post;
-		 
-		    return null;
+			$post = $wpdb->get_row($wpdb->prepare("SELECT * FROM $wpdb->posts WHERE post_title = %s AND post_type='post'", $page_title), $output);
+
+			if ($post)
+				return $post;
+
+			return null;
 		}
 		
 		/**
@@ -61,32 +69,37 @@
 		
 		/**
 		 * Déterminer si il sagit d'un custom post type
+		 *
 		 * @param string $type
+		 *
 		 * @return boolean
 		 */
-		public static function isCustomPostType($type = '') 
+		public static function isCustomPostType($type = '')
 		{
 			global $post;
 			$post_type = get_post_type($post);
-			$types = array("post", "page", "revision", "attachment");
+			$types     = ["post", "page", "revision", "attachment"];
+
 			if ($type == '' && !in_array($type, $types)) {
 				return true;
-			}else if ($type == $post_type) {
+			} else if ($type == $post_type) {
 				return true;
-			}else {
+			} else {
 				return false;
 			}
 		}
-		
+
 		/**
 		 * Remove <p> tag from images in the_content()
 		 */
 		public static function filterPTagsOnImages()
 		{
-			function filter_ptags_on_images($content){
+			function dfwp_filter_ptags_on_images($content)
+			{
 				return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
-				add_filter('the_content', 'filter_ptags_on_images');
+				add_filter('the_content', 'dfwp_filter_ptags_on_images');
 			}
 		}
 	}
-?>
+
+	?>
--- Doublefou/Helper/Theme.php
+++ Doublefou/Helper/Theme.php
@@ -1,10 +1,11 @@
-<?php 
+<?php
 	
 	namespace Doublefou\Helper;
+
 	use Doublefou\Core\Singleton;
 
 	//Exit si accès direct
-	if (!defined('ABSPATH')) exit; 
+	if (!defined('ABSPATH')) exit;
 
 	/**
 	 * Theme
@@ -17,15 +18,16 @@
 		 */
 		public static function addPostThumbnails()
 		{
-			if(function_exists('add_theme_support')){ 
-				add_theme_support('post-thumbnails'); 
+			if (function_exists('add_theme_support')) {
+				add_theme_support('post-thumbnails');
 			}
 		}
 
 		/**
 		 * Nétoyer le header html
 		 */
-		public static function cleanHeader(){
+		public static function cleanHeader()
+		{
 			remove_action('wp_head', 'rsd_link');
 			remove_action('wp_head', 'wlwmanifest_link');
 			remove_action('wp_head', 'index_rel_link');
@@ -34,18 +36,19 @@
 
 		/**
 		 * Désactiver la barre d'administration sur le front pour tous les users
+		 *
 		 * @param  string $pNotForRole Rôle d'utilisateur en exception
 		 */
 		public static function hideAdminBar($pNotForRole = '')
 		{
-			if(!empty($pNotForRole)){
-				if(!current_user_can($pNotForRole)){
-					add_filter('show_admin_bar',function() {
+			if (!empty($pNotForRole)) {
+				if (!current_user_can($pNotForRole)) {
+					add_filter('show_admin_bar', function () {
 						return false;
 					});
 				}
-			}else{
-				add_filter('show_admin_bar',function() {
+			} else {
+				add_filter('show_admin_bar', function () {
 					return false;
 				});
 			}
@@ -61,7 +64,7 @@
 			if (function_exists('wp_get_theme'))
 				$current_theme = wp_get_theme();
 			elseif (function_exists('get_theme_data'))
-				$current_theme = get_theme_data( get_stylesheet_directory_uri() .'/style.css' );
+				$current_theme = get_theme_data(get_stylesheet_directory_uri() . '/style.css');
 			else
 				return null;
 			
@@ -73,7 +76,7 @@
 		 */
 		public static function removeHeaderRssLink()
 		{
-			remove_action( 'wp_head', 'feed_links_extra', 3 );
+			remove_action('wp_head', 'feed_links_extra', 3);
 		}
 
 		/**
@@ -81,25 +84,30 @@
 		 */
 		public static function removeHeaderShortLink()
 		{
-			remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
+			remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
 		}
 
 		/**
 		 * Buffer pour la fonction language_attributes()
+		 *
 		 * @param  string $doctype
+		 *
 		 * @return string
 		 */
-		public static function getLanguageAttributes($doctype='html'){
+		public static function getLanguageAttributes($doctype = 'html')
+		{
 			ob_start();
 			language_attributes($doctype);
+
 			return ob_get_clean();
 		}
 
 		/**
 		 * Désactiver les EMOJI
 		 */
-		public static function disableEmoji(){
-			add_action( 'init',function(){			
+		public static function disableEmoji()
+		{
+			add_action('init', function () {
 				remove_action('admin_print_styles', 'print_emoji_styles');
 				remove_action('wp_head', 'print_emoji_detection_script', 7);
 				remove_action('admin_print_scripts', 'print_emoji_detection_script');
@@ -107,14 +115,15 @@
 				remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
 				remove_filter('the_content_feed', 'wp_staticize_emoji');
 				remove_filter('comment_text_rss', 'wp_staticize_emoji');
-				add_filter('tiny_mce_plugins', function($plugins){
-					if (is_array($plugins)){
-						return array_diff($plugins, array('wpemoji'));
-					}else {
-						return array();
+				add_filter('tiny_mce_plugins', function ($plugins) {
+					if (is_array($plugins)) {
+						return array_diff($plugins, ['wpemoji']);
+					} else {
+						return [];
 					}
 				});
 			});
 		}
 	}
+
 ?>
