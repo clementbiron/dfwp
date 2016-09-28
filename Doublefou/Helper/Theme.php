af1c7e3a4ce9b0f3748b9722a00d8a90f34494d3
Clément Biron <clement.biron@gmail.com>
2016-09-28 16:05:36 +0200

    modifis gitignore modifs de Theme::cleanHeader

--- Doublefou/Helper/Theme.php
+++ Doublefou/Helper/Theme.php
@@ -32,6 +32,9 @@
 			remove_action('wp_head', 'wlwmanifest_link');
 			remove_action('wp_head', 'index_rel_link');
 			remove_action('wp_head', 'wp_generator');
+			remove_action('wp_head', 'rest_output_link_wp_head');
+			remove_action('wp_head', 'wp_oembed_add_discovery_links');
+			remove_action('wp_head', 'wp_resource_hints', 2 );
 		}
 
 		/**
